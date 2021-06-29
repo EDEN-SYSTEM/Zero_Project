9b03b0ee55ed9c1f7406df9234761a4856a2345a
EDEN-SYSTEM <MyCuteWolf@yandex.ru>
2021-06-29 17:07:32 +0300

    Модель-представления приложения

---
+++ Zero_Project/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Zero_Project.ViewModels.Base;
+using System;
+
+namespace Zero_Project.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Hello, World!";
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+    }
+}
