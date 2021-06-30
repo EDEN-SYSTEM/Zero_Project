70d861ed34798df8f7931315b94c10aa146e1701
EDEN-SYSTEM <MyCuteWolf@yandex.ru>
2021-06-30 10:27:40 +0300

    Базовый класс интерфейса  ICommand

---
+++ Zero_Project/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Zero_Project.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /// <summary>
+        /// Задачей данного метода является определение доступности привязанной к нему комманды на ее выполнение.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public abstract bool CanExecute(object parameter);
+
+        /// <summary>
+        /// Задачей данного метода является выполнение тела команды, 
+        /// которая привязана к данному методу
+        /// </summary>
+        /// <param name="parameter"></param>
+        public abstract void Execute(object parameter);
+    }
+}
